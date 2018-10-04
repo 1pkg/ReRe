bf9a349c3e3a66622312d1a82274bf7ab4f4f72c
John Brown <alfa0dude@gmail.com>
2018-10-04 20:13:21 +0300

    client: fix nginx scripts.

--- client/docker/client/scripts/entry.sh
+++ client/docker/client/scripts/entry.sh
@@ -1,25 +1,15 @@
 #!/bin/sh
 
-if [ ! -f /etc/nginx/certificate/dhparams.pem ]; then
-    openssl dhparam -out /etc/nginx/certificate/dhparams.pem 2048
-    chmod 600 /etc/nginx/certificate/dhparams.pem
-fi
-
 (
- sleep 15 &
+ sleep 30 &
  while :
  do
-
-    rm /etc/nginx/conf.d/*
-    envsubst '$$HOST' < /etc/nginx/templates/challenge.conf > /etc/nginx/conf.d/challenge.conf
-    nginx -s reload
-
     /etc/nginx/scripts/letsencrypt.sh
 
     rm /etc/nginx/conf.d/*
-    envsubst '$$HOST' < /etc/nginx/templates/client.conf > /etc/nginx/conf.d/client.conf
-    envsubst '$$HOST, $$API_HOST' < /etc/nginx/templates/api.conf > /etc/nginx/conf.d/api.conf
     envsubst '$$HOST, $$LOOK_UP_HOST' < /etc/nginx/templates/lookup.conf > /etc/nginx/conf.d/lookup.conf
+    envsubst '$$HOST, $$API_HOST' < /etc/nginx/templates/api.conf > /etc/nginx/conf.d/api.conf
+    envsubst '$$HOST' < /etc/nginx/templates/client.conf > /etc/nginx/conf.d/client.conf
     envsubst '$$HOST' < /etc/nginx/templates/_deny.conf > /etc/nginx/conf.d/_deny.conf
     nginx -s reload
 
--- client/docker/client/scripts/letsencrypt.sh
+++ client/docker/client/scripts/letsencrypt.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
+if [ ! -f /etc/nginx/certificate/dhparams.pem ]; then
+    openssl dhparam -out /etc/nginx/certificate/dhparams.pem 2048
+    chmod 600 /etc/nginx/certificate/dhparams.pem
+fi
+
 if [ -f /etc/nginx/certificate/cert.pem ] && openssl x509 -checkend 2592000 -noout -in /etc/nginx/certificate/cert.pem ; then
     return 1
 fi
+
+rm /etc/nginx/conf.d/*
+envsubst '$$HOST' < /etc/nginx/templates/challenge.conf > /etc/nginx/conf.d/challenge.conf
+nginx -s reload
 
 certbot certonly -t -n --agree-tos --renew-by-default --email "${ENCRYPT_EMAIL}" --webroot -w /usr/share/nginx/html -d ${ENCRYPT_DOMAIN}
 le_result=$?
